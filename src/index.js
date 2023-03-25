adf086b5bca9b3afb98ba892b53d6fe85d13424c
Captainlomik <nastiy248@mail.ru>
2023-03-25 22:15:01 +0300

    add confetti

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./scss/style.scss";
 import gsap from "gsap";
+import { TweenMax } from "gsap/gsap-core";
 
 let cat = document.querySelector(".cat1");
 let tail = document.querySelector(".cat1__tail");
+let txt = document.querySelector(".card__txt");
 
 //Глаза
 document.onmousemove = function (event) {
@@ -29,7 +31,6 @@ cat.onmousemove = function (event) {
     x: mouseX * 4,
     rotation: mouseX * 4,
   });
-  console.log(mouseX - 2);
 };
 cat.addEventListener("mouseout", (event) => {
   gsap.to(tail, 1, {
@@ -39,13 +40,72 @@ cat.addEventListener("mouseout", (event) => {
 });
 
 //Текст
-let txt = gsap.from("h1",
-  {
-    rotationY: 36,
-    opacity: 0,
-    duration: 3,
-    yPercent: -100,
-    stagger: 0.1,
-    ease: "elastic.out(1, 0.3)"
-  })
+gsap.from("h1", {
+  rotationY: 36,
+  opacity: 0,
+  duration: 3,
+  yPercent: -100,
+  stagger: 0.1,
+  ease: "elastic.out(1, 0.3)",
+});
 
+//Конфети
+let total = 50;
+let container = document.querySelector(".card");
+let btn = document.querySelector("button");
+
+btn.addEventListener("mouseover", confetti);
+btn.addEventListener("click", deleteConf);
+
+function deleteConf() {
+  let conf = document.querySelectorAll('.dot')
+  for (let i in conf) {
+    conf[i].parentNode.removeChild(conf[i])
+  }
+}
+
+function confetti() {
+
+  let w = container.offsetWidth;
+  let h = container.offsetHeight;
+
+  for (var i = 0, div; i < total; i++) {
+    div = document.createElement("div");
+    div.classList.add("dot");
+    container.insertBefore(div, txt);
+
+    gsap.set(div, {
+      x: R(0, w),
+      y: R(-100, 100),
+      opacity: 1,
+      scale: R(0, 0.5) + 0.5,
+      backgroundColor: "hsl(" + R(170, 360) + ",50%,50%)",
+    });
+    animm(div);
+  }
+
+  function animm(elm) {
+    gsap.to(elm, R(0, 5) + 3, {
+      y: h,
+      // ease: Linear.easeNone,
+      repeat: -1,
+      delay: -5,
+    });
+    gsap.to(elm, R(0, 5) + 1, {
+      x: "+=70",
+      repeat: -1,
+      yoyo: true,
+      // ease: Sine.easeInOut,
+    });
+    gsap.to(elm, R(0, 1) + 0.5, {
+      opacity: 0,
+      repeat: -1,
+      yoyo: true,
+      // ease: Sine.easeInOut,
+    });
+  }
+
+  function R(min, max) {
+    return min + Math.random() * (max - min);
+  }
+}
