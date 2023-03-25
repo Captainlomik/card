6d9efb72d423c3644eaed193f8e94f044d3058be
Captainlomik <nastiy248@mail.ru>
2023-03-25 16:30:16 +0300

    add hat + tail + txt

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
-import './scss/style.scss';
-import gsap from 'gsap';
+import "./scss/style.scss";
+import gsap from "gsap";
 
+let cat = document.querySelector(".cat1");
+let tail = document.querySelector(".cat1__tail");
+
+//Глаза
 document.onmousemove = function (event) {
-    let x = event.x - 208;
-    let y = event.y - 368;
+  let x = event.x - 208;
+  let y = event.y - 368;
 
-    document.querySelector('.eye__l').style.transform = 'rotate(' + 57.2958 * argctg(x, y) + 'deg)';
-    document.querySelector('.eye__r').style.transform = 'rotate(' + 57.2958 * argctg(x-100, y) + 'deg)';
+  document.querySelector(".eye__l").style.transform =
+    "rotate(" + 57.2958 * argctg(x, y) + "deg)";
+  document.querySelector(".eye__r").style.transform =
+    "rotate(" + 57.2958 * argctg(x - 100, y) + "deg)";
 
-    function argctg(x, y) {
-       if( x > 0 && y > 0) return Math.PI / 2 - Math.atan(x / y)
-       if( x < 0 && y > 0) return Math.PI / 2 - Math.atan(x / y)
-       if( x < 0 && y < 0) return Math.PI + Math.atan(y / x)
-       if( x > 0 && y < 0) return 3 * Math.PI / 2 + Math.abs(Math.atan(x / y))
-    }
-}
+  function argctg(x, y) {
+    if (x > 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+    if (x < 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+    if (x < 0 && y < 0) return Math.PI + Math.atan(y / x);
+    if (x > 0 && y < 0) return (3 * Math.PI) / 2 + Math.abs(Math.atan(x / y));
+  }
+};
 
-// let eye = document.querySelector('.eye__l')
-// console.log(eye.getBoundingClientRect())
 
-let cat = document.querySelector('.cat1')
+// Хвост
+cat.onmousemove = function (event) {
+   let mouseX = (event.clientX / cat.offsetWidth) * 2
+  gsap.to(tail, 1, {
+      x: mouseX * 4,
+      rotation: mouseX * 4,
 
-cat.onmousemove = function(event){
-    document.querySelector('.cat1__tail').style.transform = ''
-}
-
-gsap.to(".cat1" , {
-    borderRadius: 50,
-    x: 100,
-    scale: 1.5,
-    duration:1
-  })
+  });
+    console.log( mouseX -2  )
+};
+cat.addEventListener("mouseout", (event) => {
+  gsap.to(tail, 1, {
+    x: 0,
+      rotation: 0,
+  });
+});
